37b445b17196bf4036de9cd51a5eac67ed4121ef
Lars Hoss <lars.hoss@gmail.com>
2012-04-08 21:48:39 +0200

    Playing around with jQuery 1.7

--- static/js/input-pimp.js
+++ static/js/input-pimp.js
@@ -1,3 +1,18 @@
-YUI.add('input-pimp', function(Y) {
-	console.log('input-pimp');
-});
+(function($) {
+	$.fn.inputPimp = function() {
+		this.focusin(function(e) {
+			$(this).prev().toggleClass('focus');
+		});
+		this.focusout(function(e) {
+			$(this).prev().toggleClass('focus');
+		});
+		this.keydown(function(e) {
+			console.log('keypress');
+			// If there is some text do hide the label
+			var _this = $(this);
+			setTimeout(function() {
+				_this.prev().css('display', e.target.value.length > 0 ? 'none' : 'block');
+			}, 0);
+		});
+	};
+})(jQuery);
